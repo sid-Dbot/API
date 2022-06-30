394d00e15ccb76884c27ae8dfcad3757de17266e
sid <s.sidd707@gmail.com>
2022-06-30 18:04:47 +0545

    Add project files.

---
+++ API/Controllers/UsersControler.cs
@@ -0,0 +1,33 @@
+﻿using API.Data;
+using API.Entity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace API.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersControler : ControllerBase
+    {
+
+        private readonly DataContext _context;
+        public UsersControler(DataContext Context)
+        {
+            _context = Context; 
+            
+    }
+        [HttpGet]
+        public ActionResult<IEnumerable<User>> GetUsers()
+        {
+            return _context.Users.ToList();
+           
+
+        }
+
+        [HttpGet("{id}")]
+        public ActionResult<User> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+            
+        }
+    }
+}
