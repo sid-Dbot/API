771efc90ddd70e1b9f1fafc3fa78fb95af175048
sid <s.sidd707@gmail.com>
2022-06-30 18:04:47 +0545

    Add project files.

---
+++ API/Data/dataContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace API.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Entity.User> Users { get; set; }
+    }
+}
---
+++ API/Entity/User.cs
@@ -0,0 +1,10 @@
+﻿namespace API.Entity
+{
+    public class User
+    {
+        
+        public int Id { get; set; }
+        public String UserName { get; set; }
+
+    }
+}
